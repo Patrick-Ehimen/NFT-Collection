411f752823ca522488214126d673e3ea040d74f0
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-13 23:20:50 +0100

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x37c33610E8FEd571d87931a210bf15071aCd1078";
+const WHITELIST_CONTRACT_ADDRESS = "0x7ceCC0C455FBF1ccd2497834dd711E22AebE21BD";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-psi-indol.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
